a1ccea3fe8b592f9485ed531afd85ff3876477e8
Matthias404 <matthias.muehl93@gmail.com>
2018-04-01 00:01:38 +0200

    Aufgaben1.1_1.2

---
+++ 1_Stunde/Aufgabe1.1/Aufgabe1.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe1._1
+{
+    class Program
+    {   public static String w1;
+        public static String k1;
+        public static String o1;
+        static void Main(string[] args)
+        {
+            
+           
+            int a;
+            
+            Console.WriteLine("Bitte w für Würfel, k, Kugel oder o für Oktaeder eingeben");                     
+            string zeile =Console.ReadLine();
+            Console.WriteLine("bitte Wert eingeben");
+            string wert = Console.ReadLine();
+
+            a = int.Parse(wert);
+            double pi = 3.14;
+
+            string w1 = "w";
+            string k1 = "k";
+            string o1 = "o";
+
+            if (zeile == w1)
+            {
+                Console.WriteLine("die Oberfläche des Würfels ist "+ a*a*6);
+                Console.WriteLine("das Volumen des Würfels ist " + a * a * a);
+                Console.ReadLine();
+            }
+
+            if (zeile == k1)
+            {
+                Console.WriteLine("die Oberfläche der Kugel ist " + pi*a*a);
+                Console.WriteLine("das Volumen der Kugel ist " + (pi*a*a*a)/6);
+                Console.ReadLine();
+            }
+
+            if (zeile == o1)
+            {
+                double sqrt3 = Math.Sqrt(3);
+                double sqrt2 = Math.Sqrt(2);
+                Console.WriteLine("die Oberfläche des Oktaeders ist "+ 2*sqrt3*a*a);
+                Console.WriteLine("das Volumen des Würfels ist " + (sqrt2*a*a*a)/3);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ 1_Stunde/Aufgabe1.2/Aufgabe1.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe1._2
+{
+    class Program
+    {
+            static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+            static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+            static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+            
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+
+            while ( i <= 4) {
+
+                zufalls("");                
+                i = i+1; }
+            Console.ReadLine();
+        }
+        static void zufalls(string ausgabe)
+        {
+            int sublength = subjects.Length;
+            int verblength = verbs.Length;
+            int objlength = verbs.Length;
+
+            
+            Random rnd = new Random(); 
+
+            int sub = rnd.Next(0, sublength);
+            int verb = rnd.Next(0, verblength);
+            int obj = rnd.Next(0, objlength);
+
+            Console.WriteLine("" + subjects[sub] + " " + verbs[verb] + " " + objects[obj]);
+        }
+               }
+        }
