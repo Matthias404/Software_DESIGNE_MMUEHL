ac9c599c7e007a8809fc79a9a670ee6328a6882f
Matthias404 <matthias.muehl93@gmail.com>
2018-03-31 23:56:27 +0200

    Aufgaben

---
+++ 2_Stunde/erste anwendung/erste anwendung/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace erste_anwendung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horst;
+
+            horst = 42;
+
+            Console.WriteLine("Horst Mal Zwei" + horst * 2);
+        }
+    }
+}
