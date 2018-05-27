5eb77797876e30ed2878e617ec5837efdee00757
Matthias404 <matthias.muehl93@gmail.com>
2018-05-27 21:21:47 +0200

    aufgabe6

---
+++ 5_stunde/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aufgabe_UML
+{
+    class Program
+    {
+        static void Main(string[] args) { 
+       
+    }
+
+    class Person
+    {
+        public string Name;
+        public int Alter;
+    }
+
+    class Teilnehmer : Person
+    {
+        public int Matrikelnummer;
+
+        public void Teilnehmen()
+        {
+            
+        }
+
+       
+    }
+
+    class Dozent : Person
+    {
+        public string Büro;
+       
+
+        
+    }
+
+    class Kurs : Dozent
+    {
+        public string Titel;
+        public string Uhrzeit;
+        public string Wochentag;
+        public string Raum;
+        public int Teilnehmer;
+        public List<int> AllTeilnehmer = new List<int>();
+
+    }
+    class Sprechstunde : Kurs
+    {
+
+        public void SprechstundeInfo()
+        {
+            Console.WriteLine("Die Sprechstunde " + Uhrzeit + " findet am " + Wochentag + " in Raum " + Raum + " statt.");
+        }
+
+    }
+
+}
